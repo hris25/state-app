e5c959bbe21823f4bf1438e4d126031d13f977aa
hris25 <harleysmg753@gmail.com>
2026-03-17 10:27:03 +0100

    update script

--- api/app.js
+++ api/app.js
@@ -21,6 +21,6 @@ app.use("/api/test", testRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 
-app.listen(8800, () => {
+app.listen(3000, () => {
   console.log("Server is running!");
 });
