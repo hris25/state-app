1d337edd0fb0124eb3ca2682b3ef97075b68e6ae
hris25 <harleysmg753@gmail.com>
2026-03-17 11:15:04 +0100

    change api url in client

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://state-app-dmo7.onrender.com/api",
   withCredentials: true,
 });
 
